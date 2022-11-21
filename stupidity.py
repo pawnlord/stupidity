609554d91520758307f50f2370fdc473bf56c780
pawnlord <scruffduff321@gmail.com>
2022-11-20 21:36:12 -0500

    Add tree representation of commits to config

--- stupidity.py
+++ stupidity.py
@@ -56,7 +56,79 @@ def get_file_hash(file, buf_size=2048):
     while data:
         sha1.update(data.encode('utf-8'))
         data = file.read(buf_size)
+    file.seek(0)
     return sha1
+
+class CommitNode:
+    def __init__(self, name, parent=None):
+        self.parent = parent
+        self.name = name
+        self.children = []
+    def add_child(self, node_name : str):
+        self.children.append(CommitNode(node_name, self))
+        return self.children[-1]
+    def add_child_node(self, node):
+        self.children.append(node)
+        node.set_parent(self)
+    def set_parent(self, node):
+        self.parent = node
+
+class CommitTree: 
+    def __init__(self, current, data):
+        self.edges = [(edge.split('|')[0], edge.split('|')[1]) if len(edge)>1 else (None,None) for edge in data.split(',')]
+        self.tree_map = {}
+        for edge in self.edges: # assume root first
+            if edge[0] == None:
+                continue
+            if edge[0] == "":
+                if edge[1] in self.tree_map.keys():
+                    self.tree_map["root"] = self.tree_map[edge[1]]
+                else:    
+                    self.tree_map["root"] = CommitNode(edge[1], None)
+            else:
+                if not edge[0] in self.tree_map.keys():
+                    self.tree_map[edge[0]] =  CommitNode(edge[0], None)
+    
+                if edge[1] in self.tree_map.keys():
+                    self.tree_map[edge[0]].add_child_node(self.tree_map[edge[1]])
+                else:
+                    self.tree_map[edge[1]] = self.tree_map[edge[0]].add_child(edge[1])
+        if not "root" in self.tree_map.keys():
+            self.tree_map["root"] = CommitNode(current, None)
+    #test
+    def add_hash(self, current_hash, hash):
+        if not current_hash in self.tree_map.keys():
+            self.tree_map[current_hash] = CommitNode(current_hash, None)
+        self.tree_map[current_hash].add_child(hash)
+    def encode(self):
+        repr_list = []
+        for name, node in self.tree_map.items():
+            if node == self.tree_map["root"] and name != "root":
+                continue
+            if name == "root":
+                repr_list.append("|{}".format(node.name))
+            for child in node.children:
+                repr_list.append("{}|{}".format(node.name, child.name))
+        return ",".join(repr_list)
+class FileData:
+    def __init__(self, data):
+        self.data = data
+        self.tree = CommitTree(data["hash"] if "hash" in data.keys() else "", data["tree"] if "tree" in data.keys() else "")
+        self.time_added = data["time_added"] if "time_added" in data.keys() else str(time.time())
+    def clean_up(self):
+        self.data["tree"] = self.tree.encode()
+    def add_file(self, file, hash = None):
+        if not hash:
+            hash =  get_file_hash(file).hexdigest();
+        
+        self.tree.add_hash(self.data["hash"], hash)
+        self.data =  {
+            "time_added" : str(time.time()) if not "time_added" in self.data.keys() else self.data["time_added"],
+            "time_modified" : str(time.time()),
+            "hash" : hash
+        }
+            
+
 class StupidityRepo:
     def __init__(self):
         self.config = configparser.ConfigParser()
@@ -65,7 +137,7 @@ class StupidityRepo:
             self.config.read_file(configfile)
 
         self.tracked_filenames = getval("FILES", "tracked", self.config).split(',')
-        self.tracked_files = {"FILE:" + name:getdict("FILE:" + name, self.config) for name in self.tracked_filenames} 
+        self.tracked_files = {"FILE:" + name:FileData(getdict("FILE:" + name, self.config)) for name in self.tracked_filenames} 
         print(self.tracked_files)
         
     
@@ -79,33 +151,43 @@ class StupidityRepo:
         pass
     
     def close(self):
+        for filename, file in self.tracked_files.items():
+            file.clean_up()
+            self.config[filename] = file.data
         self.config["FILES"]["tracked"] = ",".join(self.tracked_filenames)
         with open('.stupidity/stpd.cfg', 'w') as configfile:
             self.config.write(configfile)
-    
-    def add_file(self, filename : str, file ):
+    def add_file(self, filename : str, file):
+        # add data to commite file
+        path = ".stupidity/{}".format(get_file_hash(file).hexdigest())
+        print(get_file_hash(file).hexdigest())
+        os.makedirs(path)
+        with open(path + "/" + filename, "w+") as commit_file:
+            data = file.read()
+            commit_file.write(data)
+        file.seek(0)
+        
+
+    def add_file_data(self, filename : str, file ):
         if not filename in self.tracked_filenames:
             self.tracked_filenames.append(filename)
-        
-            self.tracked_files["FILE:" + filename] = {
-                            "time_added" : str(time.time()),
-                            "time_modified" : str(time.time()),
-                            "hash" : get_file_hash(file).hexdigest()
-                        }
-            self.config["FILE:" + filename] = self.tracked_files["FILE:" + filename]
-        
+            if "FILE:" + filename in self.tracked_files.keys():
+                self.tracked_files["FILE:" + filename].add_file(file)
+            else:
+                self.tracked_files["FILE:" + filename] = FileData({ 
+                        "time_added" : str(time.time()),
+                        "time_modified" : str(time.time()),
+                        "hash" : get_file_hash(file).hexdigest,
+                    })
+            self.add_file(filename, file)
         else:
             file_data = self.tracked_files["FILE:" + filename]
             hash = get_file_hash(file).hexdigest()
-        
-            if hash != file_data["hash"]:
-                self.tracked_files["FILE:" + filename] = {
-                                "time_added" : getval("FILE:" + filename, "time_added", self.tracked_files, str(time.time())),
-                                "time_modified" : str(time.time()),
-                                "hash" : get_file_hash(file).hexdigest()
-                            }
-                self.config["FILE:" + filename] = self.tracked_files["FILE:" + filename]
-
+            print(hash + "\n" + file_data.data["hash"])
+            if hash != file_data.data["hash"]:
+                file_data.add_file(file, hash)
+                self.add_file(filename, file)
+            
 
 def main(args):
     print (args)
@@ -126,7 +208,7 @@ def main(args):
                 print("[stupidity] {}: Not a file".format(filename))
                 exit()
             with open(filename, 'r+') as file:
-                repo.add_file(filename, file)
+                repo.add_file_data(filename, file)
             idx, filename = repo.getnext(args, idx, verbose=False)
     print(repo.tracked_files)
     print(repo.tracked_filenames)
